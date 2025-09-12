b9a89039b0bc649862fe682306d6651825677285
Ercoman2 <86063715+Ercoman2@users.noreply.github.com>
2025-09-12 21:21:26 +0200

    Update transcribe2.py

--- transcribe2.py
+++ transcribe2.py
@@ -6,6 +6,7 @@ from google.oauth2 import service_account
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.http import MediaIoBaseDownload
 from googleapiclient.http import MediaFileUpload
 
